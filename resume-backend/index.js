b49b29c3c5ef7f5f93d11735305d3db0f16e7477
annierome1 <112560966+annierome1@users.noreply.github.com>
2025-01-17 10:56:59 -0500

    fixed bcrypt

--- resume-backend/index.js
+++ resume-backend/index.js
@@ -3,7 +3,7 @@ import express from 'express';
 import cors from 'cors';
 import path from 'path';
 import mongoose from 'mongoose';
-import bcrypt from 'bcrypt';
+import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 import dotenv from 'dotenv';
 import { fileURLToPath } from 'url';
